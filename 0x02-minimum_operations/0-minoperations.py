36af11ee88ca0c8f758ec734d8552d481dfbf076
EyobABN <eyob.alamerew.belay@gmail.com>
2023-02-11 22:51:39 +0300

    Add potential solution

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,7 +1,24 @@
 #!/usr/bin/python3
-""" module for a function that returns the minimum number of operations """
+""" module for a minOperations function """
+
+
+def smallestFactor(n):
+    """ returns the smallest prime number that can divide the number n """
+    for i in range(2, int(n+1)):
+        if (n % i == 0):
+            return i
 
 
 def minOperations(n):
-    numberOfOperations = 0
-    return numberOfOperations
+    """ returns the minimum number of operations """
+    sF = smallestFactor(n)
+    if sF == n:
+        return 0
+
+    ops = 0
+    while (n > 1):
+        sF = smallestFactor(n)
+        ops = ops + sF
+        n = n / sF
+
+    return ops
