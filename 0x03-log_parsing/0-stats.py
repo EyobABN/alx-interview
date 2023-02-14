0a3a280aff34c185ed21463ecffd2ed5a61e4310
EyobABN <eyob.alamerew.belay@gmail.com>
2023-02-14 23:42:58 +0300

    Add crappy implementation of SIGINT handling

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -2,7 +2,11 @@
 """ Module for Log Parser """
 
 
+import signal
 import sys
+import os
+
+
 """
 import ipaddress
 
@@ -15,36 +19,44 @@ def is_ipv4(string):
         return False
         """
 
+def handler(signum, frame):
+    print(f"File size: {total_size}")
 
-if __name__ == "__main__":
-    counter = 0
-    total_size = 0
-    codes = {}
-    for line in sys.stdin:
-        date_sep = line.split(' - [')
-        ip = date_sep[0]
-        date_rest = date_sep[1].split('] "')
-        date = date_rest[0]
-        get_rest = date_rest[1].split('"')
-        get = get_rest[0]
-        sc_fs = get_rest[1].split()
-        sc = sc_fs[0]
-        fs = sc_fs[1]
+    """ print sorted codes """
+    for code in sorted(codes.keys()):
+        print(f"{code}: {codes[code]}")
+    raise KeyboardInterrupt
 
-        """ increment total_size """
-        total_size += int(fs)
+signal.signal(signal.SIGINT, handler)
 
-        """ update codes with new status code """
-        if sc in codes:
-            codes[sc] += 1
-        else:
-            codes[sc] = 1
+counter = 0
+total_size = 0
+codes = {}
+for line in sys.stdin:
+    date_sep = line.split(' - [')
+    ip = date_sep[0]
+    date_rest = date_sep[1].split('] "')
+    date = date_rest[0]
+    get_rest = date_rest[1].split('"')
+    get = get_rest[0]
+    sc_fs = get_rest[1].split()
+    sc = sc_fs[0]
+    fs = sc_fs[1]
 
-        """ increment counter and print stats on every 10th or on Ctrl-C """
-        counter += 1
-        if (counter % 10 == 0):
-            print(f"File size: {total_size}")
+    """ increment total_size """
+    total_size += int(fs)
 
-            """ print sorted codes """
-            for code in sorted(codes.keys()):
-                print(f"{code}: {codes[code]}")
+    """ update codes with new status code """
+    if sc in codes:
+        codes[sc] += 1
+    else:
+        codes[sc] = 1
+
+    """ increment counter and print stats on every 10th or on Ctrl-C """
+    counter += 1
+    if (counter % 10 == 0):
+        print(f"File size: {total_size}")
+
+        """ print sorted codes """
+        for code in sorted(codes.keys()):
+            print(f"{code}: {codes[code]}")
