d37352c111b0a16b346b7136eb25fbb7360f2417
EyobABN <eyob.alamerew.belay@gmail.com>
2023-02-15 22:51:34 +0300

    Add task 0 solution

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -3,4 +3,18 @@
 
 
 def canUnlockAll(boxes):
-    pass
+    numberOfBoxes = len(boxes)
+    expectedKeys = []
+    foundKeys = []
+    for i in range(1, numberOfBoxes):
+        expectedKeys.append(i)
+    for boxNumber, box in enumerate(boxes):
+        for key in box:
+            if (key != boxNumber
+                    and key in expectedKeys
+                    and key not in foundKeys):
+                foundKeys.append(key)
+    foundKeys = sorted(foundKeys)
+    if (foundKeys == expectedKeys):
+        return True
+    return False
