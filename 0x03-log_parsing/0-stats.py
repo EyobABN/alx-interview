7f7d8e89c766584e9ac5953219949b91b247fa1a
EyobABN <eyob.alamerew.belay@gmail.com>
2023-02-15 23:45:20 +0300

    Check if the issue is number of lines

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -14,3 +14,46 @@ if (__name__ == "__main__"):
         else:
             codes[sc] = 1
         counter += 1
+    """
+    Lorem ipsum dolor sit amet, conse
+    ctetur adipiscing elit, sed do eiusmod tem
+    por incididunt ut labore et dolore magna al
+    iqua. Pretium lectus quam id leo in vitae. Li
+    bero enim sed faucibus turpis in eu mi bibe
+    ndum neque. Scelerisque fer
+    mentum dui faucibus in or
+    nare quam viverra orci. Ne
+    que gravida in fermentum et soll
+    icitudin ac orci. Sodales ne
+    que sodales ut etiam sit. Turp
+    is egestas sed tempus urna et pha
+    retra. Tortor pretium viverra sus
+    pendisse potenti nullam ac tor
+    tor vitae. Consequat id porta ni
+    bh venenatis cras sed felis. Mor
+    bi tristique senectus et netus. Ru
+    trum tellus pellentesque eu tinci
+    dunt tortor aliquam nulla facilisi. Pur
+    us faucibus ornare suspendisse sed nisi. Mae
+    cenas volutpat blandit aliquam etiam erat ve
+    lit scelerisque. Dictum fusce ut pla
+    cerat orci nulla pellentesque digniss
+    im enim. Sed id semper risus in hendrerit grav
+    ida rutrum quisque. Vestibulum sed arcu non od
+    io. Nunc non blandit massa enim nec. Vitae jus
+    to eget magna fermentum iaculis eu non diam ph
+    asellus. Dui nunc mattis enim ut tellus eleme
+    ntum sagittis vitae et. Cursus mattis molest
+    ie a iaculis at erat pellentesque. Velit ut to
+    rtor pretium viverra suspendisse. Nunc sceler
+    isque viverra mauris in aliquam sem frin
+    gilla. Mus mauris vitae ultricies leo in
+    teger. Fringilla ut morbi tincidunt augue inte
+    rdum velit. Non odio euismod lacinia at qu
+    is. Fames ac turpis egestas integer eget a
+    liquet nibh praesent. Id diam maecenas ul
+    tricies mi eget mauris pharetra et ultri
+    ces. Et pharetra pharetra massa mas
+    sa. Nunc congue nisi vitae susci
+    pit tellus. Eu ultrices vitae auctor eu augue.
+    """
