62e501a17575d6de34bae806308841f38b44abcc
EyobABN <eyob.alamerew.belay@gmail.com>
2023-02-16 01:02:30 +0300

    Try alternate method of handling keyboardInterrupt

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -2,7 +2,7 @@
 """ Module for Log Parser """
 
 
-import signal
+# import signal
 import sys
 import ipaddress
 
@@ -31,41 +31,47 @@ def is_size(string):
     return string.isdigit()
 
 
-def handler(signum, frame):
-    print("File size: {}".format(total_size))
-    for code in sorted(codes.keys()):
-        print("{}: {}".format(code, codes[code]))
-    raise KeyboardInterrupt
+# def handler(signum, frame):
+#     print("File size: {}".format(total_size))
+#     for code in sorted(codes.keys()):
+#         print("{}: {}".format(code, codes[code]))
+#     raise KeyboardInterrupt
 
 
-signal.signal(signal.SIGINT, handler)
+# signal.signal(signal.SIGINT, handler)
 
 if (__name__ == "__main__"):
     counter = 0
     total_size = 0
     codes = {}
-    for line in sys.stdin:
-        date_sep = line.split(' - [')
-        ip = date_sep[0]
-        date_rest = date_sep[1].split('] "')
-        date = date_rest[0]
-        get_rest = date_rest[1].split('"')
-        get = get_rest[0]
-        sc_fs = get_rest[1].split()
-        sc = sc_fs[0]
-        fs = sc_fs[1]
-        if (is_ipv4(ip) is False or
-                is_get(get) is False or
-                is_code(sc) is False or
-                is_size(fs) is False):
-            continue
-        total_size += int(fs)
-        if sc in codes:
-            codes[sc] += 1
-        else:
-            codes[sc] = 1
-        counter += 1
-        if (counter % 10 == 0):
-            print("File size: {}".format(total_size))
-            for code in sorted(codes.keys()):
-                print("{}: {}".format(code, codes[code]))
+
+    try:
+        for line in sys.stdin:
+            date_sep = line.split(' - [')
+            ip = date_sep[0]
+            date_rest = date_sep[1].split('] "')
+            date = date_rest[0]
+            get_rest = date_rest[1].split('"')
+            get = get_rest[0]
+            sc_fs = get_rest[1].split()
+            sc = sc_fs[0]
+            fs = sc_fs[1]
+            if (is_ipv4(ip) is False or
+                    is_get(get) is False or
+                    is_code(sc) is False or
+                    is_size(fs) is False):
+                continue
+            total_size += int(fs)
+            if sc in codes:
+                codes[sc] += 1
+            else:
+                codes[sc] = 1
+            counter += 1
+            if (counter % 10 == 0):
+                print("File size: {}".format(total_size))
+                for code in sorted(codes.keys()):
+                    print("{}: {}".format(code, codes[code]))
+    except KeyboardInterrupt:
+        print("File size: {}".format(total_size))
+        for code in sorted(codes.keys()):
+            print("{}: {}".format(code, codes[code]))
