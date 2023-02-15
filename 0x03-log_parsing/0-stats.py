c5cb88981e81c56f7c94fd443b0495c7dd148fc2
EyobABN <eyob.alamerew.belay@gmail.com>
2023-02-16 00:20:14 +0300

    Implement input checking

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -4,9 +4,31 @@
 
 import signal
 import sys
-import os
 import ipaddress
-import datetime
+
+
+def is_ipv4(string):
+    try:
+        ipaddress.IPv4Network(string)
+        return True
+    except ValueError:
+        return False
+
+
+def is_get(string):
+    if (string == "GET /projects/260 HTTP/1.1"):
+        return True
+    return False
+
+
+def is_code(string):
+    if (int(string) in [200, 301, 400, 401, 403, 404, 405, 500]):
+        return True
+    return False
+
+
+def is_size(string):
+    return string.isdigit()
 
 
 def handler(signum, frame):
@@ -32,6 +54,11 @@ if (__name__ == "__main__"):
         sc_fs = get_rest[1].split()
         sc = sc_fs[0]
         fs = sc_fs[1]
+        if (is_ipv4(ip) is False or
+                is_get(get) is False or
+                is_code(sc) is False or
+                is_size(fs) is False):
+            continue
         total_size += int(fs)
         if sc in codes:
             codes[sc] += 1
