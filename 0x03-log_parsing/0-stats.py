69c2adedbed29c173941d3f9219fd7649bb91ed5
EyobABN <eyob.alamerew.belay@gmail.com>
2023-02-15 23:39:00 +0300

    Try to shorten file even more

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -11,10 +11,3 @@ if (__name__ == "__main__"):
         total_size += int(fs)
         if sc in codes:
             codes[sc] += 1
-        else:
-            codes[sc] = 1
-        counter += 1
-        if (counter % 10 == 0):
-            print(f"File size: {total_size}")
-            for code in sorted(codes.keys()):
-                print(f"{code}: {codes[code]}")
