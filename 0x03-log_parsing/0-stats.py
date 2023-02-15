637312f4269c1134de8db58366ef71ab3407a8af
EyobABN <eyob.alamerew.belay@gmail.com>
2023-02-15 23:36:57 +0300

    Try to shorten file

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,6 +1,29 @@
 #!/usr/bin/python3
 """ Module for Log Parser """
 
+import sys
 
 if (__name__ == "__main__"):
-    pass
+    counter = 0
+    total_size = 0
+    codes = {}
+    for line in sys.stdin:
+        date_sep = line.split(' - [')
+        ip = date_sep[0]
+        date_rest = date_sep[1].split('] "')
+        date = date_rest[0]
+        get_rest = date_rest[1].split('"')
+        get = get_rest[0]
+        sc_fs = get_rest[1].split()
+        sc = sc_fs[0]
+        fs = sc_fs[1]
+        total_size += int(fs)
+        if sc in codes:
+            codes[sc] += 1
+        else:
+            codes[sc] = 1
+        counter += 1
+        if (counter % 10 == 0):
+            print(f"File size: {total_size}")
+            for code in sorted(codes.keys()):
+                print(f"{code}: {codes[code]}")
