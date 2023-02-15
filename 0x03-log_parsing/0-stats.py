a29d4e2ef4fdb8e1f733b84e3e3d65e9e1338a71
EyobABN <eyob.alamerew.belay@gmail.com>
2023-02-16 00:06:20 +0300

    Remove checking

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -9,49 +9,6 @@ import ipaddress
 import datetime
 
 
-def is_ipv4(string):
-    try:
-        ipaddress.IPv4Network(string)
-        return True
-    except ValueError:
-        return False
-
-
-def is_date(string):
-    try:
-        datetime.date.fromisoformat(string)
-        return True
-    except ValueError:
-        return False
-
-
-def is_get(string):
-    if (string == "GET /projects/260 HTTP/1.1"):
-        return True
-    return False
-
-
-def is_code(string):
-    if (int(string) in [200, 301, 400, 401, 403, 404, 405, 500]):
-        return True
-    return False
-
-
-def is_size(string):
-    return string.isdigit()
-
-
-def handler(signum, frame):
-    print(f"File size: {total_size}")
-
-    """ print sorted codes """
-    for code in sorted(codes.keys()):
-        print(f"{code}: {codes[code]}")
-    raise KeyboardInterrupt
-
-
-signal.signal(signal.SIGINT, handler)
-
 if (__name__ == "__main__"):
     counter = 0
     total_size = 0
@@ -66,11 +23,6 @@ if (__name__ == "__main__"):
         sc_fs = get_rest[1].split()
         sc = sc_fs[0]
         fs = sc_fs[1]
-        if (is_ipv4(ip) is False or
-                is_get(get) is False or
-                is_code(sc) is False or
-                is_size(fs) is False):
-            continue
         total_size += int(fs)
         if sc in codes:
             codes[sc] += 1
