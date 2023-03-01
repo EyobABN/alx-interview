f3f334ad59f7db58a3eec99df308d2cf826aff16
EyobABN <eyob.alamerew.belay@gmail.com>
2023-03-01 12:10:17 +0300

    Add solution

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -18,3 +18,20 @@ if __name__ == "__main__":
     if (N < 4):
         print("N must be at least 4")
         exit(1)
+
+    def queens(n, i, a, b, c):
+        """
+        Translation of Niklaus Wirth's solution into Python
+        source: https://en.wikipedia.org/wiki/Eight_queens_puzzle
+        """
+        if i < n:
+            for j in range(n):
+                if j not in a and i+j not in b and i-j not in c:
+                    yield from queens(n, i+1, a+[j], b+[i+j], c+[i-j])
+        else:
+            yield a
+
+    for solution in queens(N, 0, [], [], []):
+        for idx, y in enumerate(solution):
+            solution[idx] = [idx, y]
+        print(solution)
